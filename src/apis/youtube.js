3a76a7b5567a65c2dd9709630cae0f28608a30a4
Michael Mansy <michael.mansy@yahoo.ca>
2019-10-07 17:11:57 -0400

    add the search for videos

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyAYA00Bu_nnPZutATktcILR3QIV1DSkQuE';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+});
